8e644c58231f8197a4ac74365e8fc0cec4358e0d
Gabriel Lins <atngabriel@Gabriels-MacBook-Pro.local>
2021-06-14 23:34:58 -0300

    testing endpoints

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: '',
+  baseURL: 'http://6f60cb76d52d.ngrok.io/',
 })
 
 export default api;
--- src/services/dev.ts
+++ src/services/dev.ts
@@ -4,6 +4,7 @@ import api from '../config/api'
 
 const listDevs = async () => {
     try {
+        console.log('listDevs')
         return await api.get('/devs')
     } catch (err) {
         //tratar erro
