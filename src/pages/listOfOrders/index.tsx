71d258a12b605062866057e71dd5d820e91bbd49
Felipe <felipeoliveiramaia3@gmail.com>
2021-06-21 20:40:50 -0300

    criado o botão de apagar

--- src/pages/listOfOrders/index.tsx
+++ src/pages/listOfOrders/index.tsx
@@ -13,27 +13,15 @@ import './style.scss'
 export default function ListOfOrders(){
     const [modal, setModal] = useState(false);
 
-    function handleOpenModal () {
-        {setModal(true)}
-    }
-
-    function handleCloseModal () {
-        {setModal(false)}
-    }
-
     return(
         <>
-        <ReactModal isOpen={modal}> 
-            <p>Modal text!</p>
-            <button onClick={handleCloseModal}>Close Modal</button>
-        </ReactModal> 
         <div className='pagelist-order'>
         <h1>Serviços do cliente</h1>
             <div className='pagelist-order__main'>
                 <Accordion title="Title">
                     <p>Descrição</p>
                     <div className='button-div'> 
-                        <button type="button" className='button-delete' onClick={() => {handleOpenModal()}}> 
+                        <button type="button" className='button-delete' onClick={() => {}}> 
                             <FiTrash2/>
                         </button>
                     </div>
