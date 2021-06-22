9b0a9fabfbc3e197b05086bbf6acd833ad801696
Felipe <felipeoliveiramaia3@gmail.com>
2021-06-21 23:17:08 -0300

    Criando a pagina do assign

--- src/pages/assignOrder/index.tsx
+++ src/pages/assignOrder/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, FormEvent } from "react";
 
+import './style.scss'
+
 export default function AssignOrder () {
     const [duration, setDuration] = useState(0)
     const [userId, setUserId] = useState(0)
@@ -11,6 +13,27 @@ export default function AssignOrder () {
     })
 
     return (
-      <h1>Assign Order</h1>  
+      <div className="assign-order">
+            <h1>Criar OS</h1>
+            <div className='assign-order__main'>
+                <form>
+                    <div>
+                        <label>Data Final:</label>
+                        <input type="number" placeholder='Data Final'/>
+                    </div>
+                    <div>
+                        <label>Usuários:</label>
+                        <select>
+                          <option value="value">Valor1</option>
+                        </select>
+                    </div>
+                    <div className='button-create'>
+                        <button type="submit" className='button-add'> 
+                            Enviar
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,12 +4,14 @@ import { Switch, Route } from 'react-router-dom';
 import listOfOrders from '../pages/listOfOrders';
 import feedBackChat from '../pages/feedBackChat';
 import createOrder from '../pages/createOrder';
+import assignOrder from '../pages/assignOrder'
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={listOfOrders} />
     <Route path="/feedback" component={feedBackChat} />
     <Route path="/create-order" component={createOrder} />
+    <Route path="/assign-order" component={assignOrder} />
   </Switch>
 );
 
