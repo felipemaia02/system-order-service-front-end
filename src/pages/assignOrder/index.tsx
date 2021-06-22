c442abeaf5b62df48884e6d8b28b06cfabbd138d
Gabriel Lins <atngabriel@Gabriels-MacBook-Pro.local>
2021-06-21 23:02:25 -0300

    assing order

---
+++ src/pages/assignOrder/index.tsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect, FormEvent } from "react";
+
+export default function AssignOrder () {
+    const [duration, setDuration] = useState(0)
+    const [userId, setUserId] = useState(0)
+
+    let orderId:number
+
+    useEffect(() => {
+        
+    })
+
+    return (
+      <h1>Assign Order</h1>  
+    )
+}
